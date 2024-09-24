11596bcc7ef493ec551ba6f916723540e9b98cde
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2024-09-24 19:55:00 -0400

    create router

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+// Screens
+function InvitadosScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Pantalla de Invitados</Text>
+    </View>
+  );
+}
+
+function ProveedoresScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Pantalla de Proveedores</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Invitados">
+        <Stack.Screen name="Invitados" component={InvitadosScreen} />
+        <Stack.Screen name="Proveedores" component={ProveedoresScreen} />
+      </Stack.Navigator>
       <StatusBar style="light" />
-    </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screen/GuestList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const GuestList = () => {
+  return (
+    <View>
+      <Text>Lista de invitados</Text>
+    </View>
+  );
+};
+
+export default GuestList;
